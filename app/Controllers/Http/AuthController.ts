e381b474db5d21c6a6a4738dd69bb0b2b63845c6
MizouziE <90829439+MizouziE@users.noreply.github.com>
2022-02-03 11:14:09 +0000

    working toward hashed password login, hashing working, not redirection

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,13 +1,16 @@
 // import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Hash from '@ioc:Adonis/Core/Hash'
+
+
 
 export default class AuthController {
   public async login({ request, auth, response }) {
     const email = request.input('email')
-    const password = request.input('password')
+    const hashedPassword = await Hash.make(request.input('password'))
 
     await auth
       .use('web') // 👈 using sessions guard
-      .attempt(email, password)
+      .attempt(email, hashedPassword)
 
     response.redirect('users/:id')
   }
--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,5 +1,6 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, beforeSave } from '@ioc:Adonis/Lucid/Orm'
+import Hash from '@ioc:Adonis/Core/Hash'
 
 export default class User extends BaseModel {
   @column({ isPrimary: true })
@@ -17,8 +18,15 @@ export default class User extends BaseModel {
   @column()
   public email: string
 
-  @column()
+  @column({ serializeAs: null })
   public password: string
+
+  @beforeSave()
+  public static async hashPassword(user: User) {
+    if (user.$dirty.password) {
+      user.password = await Hash.make(user.password)
+    }
+  }
 
   @column()
   public address: string

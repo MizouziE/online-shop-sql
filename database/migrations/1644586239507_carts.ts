a4c3497a449ed85da66bb7997998a065e6bfa74c
MizouziE <90829439+MizouziE@users.noreply.github.com>
2022-02-11 13:41:43 +0000

    making migration

---
+++ database/migrations/1644586239507_carts.ts
@@ -0,0 +1,24 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Carts extends BaseSchema {
+  protected tableName = 'carts'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string('items').nullable()
+      table.integer('total_price').defaultTo('0')
+      table.integer('number_of_items').defaultTo('0')
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
